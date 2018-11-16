c3eccfc61d497c0f2b2cea07a07726ba657bbf07
Kommandotolken <honken_t@hotmail.com>
2018-11-16 11:11:10 +0100

    finished

--- main.cpp
+++ main.cpp
@@ -11,13 +11,13 @@
  */
 int main(int argc, const char * argv[])
 {
-	char fileName[30];
+	char fileName[30]; //array for the filename
 	if (argc < 2)
 	{
 		std::cout << "You forgot to enter a filename.";
 		return 1;
 	}
-	strcpy_s(fileName, argv[1]);
+	strcpy_s(fileName, argv[1]); //Copy the name from argv to fileName.
 	int avr;
 	int a = 0;
 	int counter = 0;
@@ -29,17 +29,18 @@ int main(int argc, const char * argv[])
 
 	myReadFile.open(fileName);
 	if (myReadFile.fail()) {
-		std::cout << "Sorry, there was an error. Press ENTER";
+		std::cout << "Sorry, there was an error opening the file. Press ENTER";
 		std::getchar();
 		exit(1);
 	}
 	while (myReadFile >> a)
 	{
+
 		if (counter >= startingSize)
 		{
 
 			arrayPtr2 = new int[counter + startingSize];
-			for (int i = 0; i < (counter); i++)
+			for (int i = 0; i < counter; i++)
 			{
 				arrayPtr2[i] = arrayPtr[i];
 
